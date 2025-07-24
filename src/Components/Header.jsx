2b08fc0fafe479b41e52250f79f9f5c7a1bd4ce9
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2025-07-24 19:12:40 +0530

    change in size of icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({isDarkMode,setIsDarkMode}) => {
         <div className="ml-auto flex  md:text-2xl">
           <span  onClick={()=>{
         setIsDarkMode(!isDarkMode)
-          }}><img className="h-8 w-8 mt-1 mr-2" src={isDarkMode?"https://cdn-icons-png.flaticon.com/128/869/869869.png":"https://cdn-icons-png.flaticon.com/128/702/702471.png"}/></span>
+          }}><img className="h-6 w-6 md:h-8 md:w-8 mt-1 mr-2" src={isDarkMode?"https://cdn-icons-png.flaticon.com/128/869/869869.png":"https://cdn-icons-png.flaticon.com/128/702/702471.png"}/></span>
           <Link
             to="skillsSection"
             smooth={true}
